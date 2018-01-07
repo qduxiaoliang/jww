8ba76fd20a8f25e15b6d3162a1c4e584adc1e181
whyuxn <whyuxn@163.com>
2018-01-07 18:35:35 +0800

    修改提示语超时时间为30秒

--- jww-ui/jww-ui-ump/js/base.js
+++ jww-ui/jww-ui-ump/js/base.js
@@ -21,8 +21,8 @@ layui.define(['jquery'], function (exports) {
         dataType: 'json',
         contentType: "application/json",
         beforeSend: function (evt, request, settings) {
-            // request.url = 'http://localhost:8089/' + request.url;
-            request.url = '/' + request.url;
+            request.url = 'http://localhost:8089/' + request.url;
+            // request.url = '/' + request.url;
         },
         dataFilter: function (result) {
             try {
@@ -55,7 +55,7 @@ layui.define(['jquery'], function (exports) {
             var loadingIndex = layer.msg('数据请求中', {
                 icon: 16,
                 shade: 0.01,
-                time: -1
+                time: 30000
             });
             return loadingIndex;
         },
