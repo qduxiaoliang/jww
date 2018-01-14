d19af88796f31c13c95477aa764f6e97c5967961
whyuxn <whyuxn@163.com>
2018-01-14 17:37:49 +0800

    fix error

--- jww-ump/jww-ump-server/src/main/java/com/jww/ump/server/controller/SysDeptController.java
+++ jww-ump/jww-ump-server/src/main/java/com/jww/ump/server/controller/SysDeptController.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
